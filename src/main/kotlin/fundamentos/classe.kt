d936de7b2e35ad26eb19e4b3c68ae30c91de10ef
Kleiton Utida <kleiton.utida@live.com>
2023-03-15 03:17:43 -0300

    Class

--- src/main/kotlin/fundamentos/classe.kt
+++ src/main/kotlin/fundamentos/classe.kt
@@ -1,5 +1,12 @@
 package fundamentos
 
-class classe {
-    
+class Pessoa(var nome: String, var idade: Int) {
+    override fun toString(): String {
+        return "Classe: Pessoa. Nome: $nome, Idade: $idade"
+    }
+}
+
+fun main() {
+    var kleiton = Pessoa("Kleiton", 35)
+    println(kleiton)
 }
