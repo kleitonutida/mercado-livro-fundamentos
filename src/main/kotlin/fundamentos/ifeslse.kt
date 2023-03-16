7137387060b216bf51d23b5820ed5276a93620c7
Kleiton Utida <kleiton.utida@live.com>
2023-03-15 23:58:36 -0300

    Structure decision

---
+++ src/main/kotlin/fundamentos/ifeslse.kt
@@ -0,0 +1,31 @@
+package fundamentos
+
+fun main() {
+//    parOuImpar(2)
+//    parOuImpar(3)
+
+    resultadoDaNota(3)
+    resultadoDaNota(5)
+    resultadoDaNota(8)
+
+    println(parOuImpar(2))
+    println(parOuImpar(9))
+}
+
+fun parOuImpar(numero: Int): String {
+    return if (numero % 2 == 0) {
+        "Par"
+    } else {
+        "Ímpar"
+    }
+}
+
+fun resultadoDaNota(nota: Int) {
+    if (nota >= 6) {
+        println("Passou")
+    } else if (nota >= 4) {
+        println("Recuperação")
+    } else {
+        println("Reprovou")
+    }
+}
