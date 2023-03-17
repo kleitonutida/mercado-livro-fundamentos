6f67e59bbc8d9be5f497b031b303ee09e18adb2f
Kleiton Utida <kleiton.utida@live.com>
2023-03-16 22:40:03 -0300

    Null Safety

---
+++ src/main/kotlin/fundamentos/nullsafety.kt
@@ -0,0 +1,20 @@
+package fundamentos
+
+fun main() {
+
+    var lista: List<Int?> = listOf(1, 2, null, 4)
+    var listaNullabel: List<Int>? = null
+
+
+//    var nome : String? = null
+//    var tamanho: Int = nome?.length ?: 0
+//    println(tamanho)
+
+//    var nome: String? = "Gustavo"
+//
+//    if (nome != null) {
+//        println(nome.length)
+//    }
+//
+//    val toShort = nome!!.length.toShort()
+}
